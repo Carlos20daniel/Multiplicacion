f0f62ffc42f8a9a87128c9ace55d8e4ee1e4d57d
Carlos20daniel <1440648@senati.pe>
2023-08-18 16:17:08 -0500

    Calculadora Virtual

--- prueba_java/src/main/java/com/mycompany/prueba_java/Prueba_java.java
+++ prueba_java/src/main/java/com/mycompany/prueba_java/Prueba_java.java
@@ -14,41 +14,71 @@ public class Prueba_java {
 
     public static void main(String[] args) {
         System.out.println("--------CALCULADORA VIRTUAL--------");
+        
         System.out.println("--------SUMA--------");
+        //INSERTAMOS UNA LIBRERIA PARA USAR LA CONSOLA
         Scanner entrada = new Scanner(System.in);
+        //CREAMOS LAS VARIABLES A USAR PARA LOS DATOS
         int a,b,suma;
-        System.out.println("ingrese el primer numero:");
+        //INGRESAMOS EL PRIMER NUMERO A SUMAR
+        System.out.println("Digite el primer numero:");
         a=entrada.nextInt();
-        System.out.println("ingrese el segundo numero:");
+        //INGRESAMOS EL SEGUNDO NUMERO A SUMAR
+        System.out.println("Digite el segundo numero:");
         b=entrada.nextInt();
+        //CREAMOS LA VARIABLE QUE SUMARA LOS DATOS
         suma = a+b;
-        System.out.println("La suma de ambos numeros es: " + suma);
+        //SALIDA DE LOS DATOS INGRESADOS Y SU RESULTADO
+        System.out.println("EL RESULTADO DE ESTA SUMA ES: " + suma);
+        
+        System.out.println("-----------------------------------");
         
         System.out.println("--------RESTA--------");
+        //CREAMOS LAS VARIABLES A USAR PARA LOS DATOS
         int c,d,resta;
-        System.out.println("ingrese el primer numero:");
+        //INGRESAMOS EL PRIMER NUMERO A RESTAR
+        System.out.println("Digite el primer numero:");
         c=entrada.nextInt();
-        System.out.println("ingrese el segundo numero:");
+        //INGRESAMOS EL SEGUNDO NUMERO A RESTAR
+        System.out.println("Digite el segundo numero:");
         d=entrada.nextInt();
+        //CREAMOS LA VARIABLE QUE RESTARA LOS DATOS
         resta = c-d;
-        System.out.println("La resta de ambos numeros es: " + resta);
+        //SALIDA DE LOS DATOS INGRESADOS Y SU RESULTADO
+        System.out.println("EL RESULTADO DE ESTA RESTA ES: " + resta);
+        
+        System.out.println("-----------------------------------");
+
         
         System.out.println("--------DIVICION--------");
+        //CREAMOS LAS VARIABLES A USAR PARA LOS DATOS
         int e,f,divicion;
-        System.out.println("ingrese el primer numero:");
+        //INGRESAMOS EL PRIMER NUMERO A DIVIDIR
+        System.out.println("Digite el primer numero:");
         e=entrada.nextInt();
-        System.out.println("ingrese el segundo numero:");
+        //INGRESAMOS EL SEGUNDO NUMERO A DIVIDIR
+        System.out.println("Digite el segundo numero:");
         f=entrada.nextInt();
+        //CREAMOS LA VARIABLE QUE DIVIDIRA LOS DATOS
         divicion = e/f;
-        System.out.println("La divicion de ambos numeros es: " + divicion);
+        //SALIDA DE LOS DATOS INGRESADOS Y SU RESULTADO
+        System.out.println("EL RESULTADO DE ESTA DIVICION ES:  " + divicion);
+        
+        System.out.println("-----------------------------------");
+
         
         System.out.println("--------MULTIPLICACION--------");
+        //CREAMOS LAS VARIABLES A USAR PARA LOS DATOS
         int g,h,multiplicacion;
-        System.out.println("ingrese el primer numero:");
+        //INGRESAMOS EL PRIMER NUMERO A MULTIPLICAR
+        System.out.println("Digite el primer numero:");
         g=entrada.nextInt();
-        System.out.println("ingrese el segundo numero:");
+        //INGRESAMOS EL SEGUNDO NUMERO A MULTIPLICAR
+        System.out.println("Digite el segundo numero:");
         h=entrada.nextInt();
+        //CREAMOS LA VARIABLE QUE MULTIPLICARA LOS DATOS
         multiplicacion = g*h;
-        System.out.println("La multiplicacion de ambos numeros es: " + multiplicacion);
+        //SALIDA DE LOS DATOS INGRESADOS Y SU RESULTADO
+        System.out.println("EL RESULTADO DE ESTA MULTIPLICACION ES:  " + multiplicacion);
     }
 }
